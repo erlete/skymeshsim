f20e5b75d8de5e77e6a73332e28886db21e2f79d
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-11-23 19:29:06 +0100

    Update control system and format code

--- src/skymeshsim/network/control_system.py
+++ src/skymeshsim/network/control_system.py
@@ -1,39 +1,77 @@
+"""Control system module.
+
+This module contains the ControlSystem class, which is responsible for sending
+user commands to the server.
+
+Author:
+    Paulo Sanchez (@erlete)
+"""
+
+
 import asyncio
 
+from .logger import Logger
 from .messages import ClientIdentificationMessage, CommandMessage
-from .network_component import _BaseNetworkComponent
+from .network_component import _BaseNetworkComponent, _NetworkInputReader
+
+COMMANDS = {
+    "help": "Show this help message.",
+    "moveto <longitude>, <latitude>": (
+        "Move to the specified longitude and latitude coordinates."
+    ),
+    "exit": "Exit the ControlSystem."
+}
 
 
-class ControlSystem(_BaseNetworkComponent):
+class ControlSystem(_BaseNetworkComponent, _NetworkInputReader):
     """Sends user commands to the server."""
 
     def __init__(self, host: str, port: int):
         super().__init__(host, port)
 
-    async def get_user_input(self) -> str:
-        """Asynchronously get user input."""
-        print("> ", end="", flush=True)
-        return await asyncio.get_event_loop().run_in_executor(None, input)
+        self._online = True
+        self._logger = Logger(0, "[ControlSystem]")
 
     async def run(self) -> None:
         """Connect to the server and send user commands."""
         _, writer = await asyncio.open_connection(self.host, self.port)
+
         await ClientIdentificationMessage(
             component="ControlSystem",
             writer=writer
         ).send()
 
-        print("ControlSystem connected. Type commands (e.g., 'moveto 10, 20').")
-        try:
-            while True:
-                command = await self.get_user_input()
-                if command.strip().lower() == "exit":
-                    print("Exiting ControlSystem...")
-                    break
+        self._logger.log("ControlSystem started.", 1)
+        self._logger.log(
+            "Type a command or 'help' to see a list of available commands.",
+            1
+        )
 
-                # Prepare the command as a JSON object
+        # Main loop:
+        try:
+            while self._online:
+                command = (await self.get_user_input()).strip().lower()
+
+                # Internal operation commands:
+                if command == "exit":
+                    self._online = False
+                    self._logger.log("Exiting ControlSystem.", 1)
+                    continue
+
+                elif command == "help":
+                    max_command_length = max(len(cmd) for cmd in COMMANDS)
+                    help_message = "Available commands:\n\n"
+                    for command, description in COMMANDS.items():
+                        help_message += (
+                            f"  {command:<{max_command_length + 10}}"
+                            + f" - {description}\n"
+                        )
+
+                    self._logger.log(help_message.strip(), 1)
+                    continue
+
+                # External operation commands:
                 if command.lower().startswith("moveto"):
-                    # Example: Move to <x>, <y>
                     await CommandMessage(
                         command="moveto",
                         target=tuple(
@@ -42,14 +80,22 @@ class ControlSystem(_BaseNetworkComponent):
                         writer=writer
                     ).send()
                 else:
-                    print("Unknown command format. Only 'CMD:' commands are allowed.")
+                    self._logger.log(
+                        "Unknown command format. Type 'help' for a list of "
+                        + "available commands.",
+                        2
+                    )
+
         except asyncio.CancelledError:
-            print("ControlSystem interrupted.")
+            self._logger.log("ControlSystem connection interrupted.", 2)
+
         finally:
             writer.close()
             await writer.wait_closed()
+            self._logger.log("ControlSystem connection closed.", 1)
 
 
 if __name__ == "__main__":
+    # A single ControlSystem instance can be run per standalone application:
     control_system = ControlSystem(host="127.0.0.1", port=8888)
     asyncio.run(control_system.run())
