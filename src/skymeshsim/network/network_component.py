c220e4049313d6c665036d8a78af9463b9e7192b
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-11-23 19:28:55 +0100

    Add networkinputreader mixin

--- src/skymeshsim/network/network_component.py
+++ src/skymeshsim/network/network_component.py
@@ -5,6 +5,9 @@ Author:
 """
 
 
+import asyncio
+
+
 class _BaseNetworkComponent:
     """Network component base class.
 
@@ -17,3 +20,12 @@ class _BaseNetworkComponent:
         """Initialize a NetworkComponent instance."""
         self.host = host
         self.port = port
+
+
+class _NetworkInputReader:
+    """Network input reader mixin class."""
+
+    async def get_user_input(self) -> str:
+        """Asynchronously get user input."""
+        print("> ", end="", flush=True)
+        return await asyncio.get_event_loop().run_in_executor(None, input)
