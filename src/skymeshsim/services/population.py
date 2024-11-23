d09f4d1fbd85169129eaca766b3466da5435939c
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2024-11-23 20:58:27 +0100

    Add services placeholder

---
+++ src/skymeshsim/services/population.py
@@ -0,0 +1,8 @@
+"""Population service module.
+
+This module provides the Population service, which is responsible for
+providing information about the population density of an area.
+
+Author:
+    Paulo Sanchez (@erlete)
+"""
---
+++ src/skymeshsim/services/terrain.py
@@ -0,0 +1,8 @@
+"""Terrain service module.
+
+This module provides the Terrain service, which is responsible for providing
+information about the terrain of an area.
+
+Author:
+    Paulo Sanchez (@erlete)
+"""
